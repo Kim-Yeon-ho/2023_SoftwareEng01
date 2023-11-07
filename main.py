5d1078529c429484b4197d81ef4a07eabec719b3
전승혁 <gainthunter@naver.com>
2023-11-07 12:30:12 +0900

    스택 구현 수정본
    리스트 스택과 linkedlist 스택 중에서 리스트 스택을 선택하였습니다.
    pop()과 top()에서 isEmpty가 True이면 return 값이 print에서 False로 변경하였습니다.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 # 4. 예외처리
 # 5. 예외처리
 # 6. 이스터에그
- 
 
 class Stack():                      #Stack 클래스 생성
     def __init__(self):             #클래스 초기화
@@ -15,8 +14,8 @@ class Stack():                      #Stack 클래스 생성
     
     def pop(self):                  #스택에서 데이터 반환
         popObject = None
-        if self.isEmpty():         
-            print("Stack is Empty")
+        if self.isEmpty():         #스택이 비어있으면 False, 있으면 반환
+            return False
         else:
             popOjbect = self.stack.pop()
         
@@ -24,8 +23,8 @@ class Stack():                      #Stack 클래스 생성
     
     def top(self):                  #스택의 가장 위에 있는 데이터 반환
         topObject = None
-        if self.isEmpty():
-            print("Stack is Empty")
+        if self.isEmpty():          #스택 최상단이 비어있으면 False, 있으면 반환
+            return False
         else:
             topObject = self.stack[-1]
         
@@ -37,47 +36,3 @@ class Stack():                      #Stack 클래스 생성
             isEmpty = True
         
         return isEmpty
-
-
-
-class Node:                         #Linked list를 위한 Node 클래스 생성
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class linkedListStack():            #linkedListStack 클래스 생성
-    def __init__(self):             #클래스 초기화
-        self.head = None
-    
-    def push(self, data):           #스택에 데이터 추가
-        newNode = Node(data)
-        newNode.next = self.head
-        self.head = newNode
-    
-    def pop(self):                  #스택에서 데이터 반환
-        popObject = None
-
-        if self.isEmpty():
-            print("Stack is Empty")
-        else:
-            popObject = self.head.data
-            self.head = self.head.next
-        
-        return popObject
-    
-    def top(self):                  #스택의 가장 위에 있는 데이터 반환
-        topObject = None
-        if self.isEmpty():
-            print("stack is Empty")
-        else:
-            topObject = self.head.data
-        
-        return topObject
-    
-    def isEmpty(self):              #스택이 비어있으면 True, 채워져있으면 False
-        isEmpty = False
-        if self.head is None:
-            isEmpty = True
-        
-        return isEmpty
- 
