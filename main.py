8a5c5dc6c8dec6c3064b90c269252efed59a8253
iiinsk <opoet1020@jbnu.ac.kr>
2023-11-13 00:37:30 +0900

    나눗셈(/) 예외처리

--- main.py
+++ main.py
@@ -4,4 +4,37 @@
 # 4. 예외처리
 # 5. 예외처리
 # 6. 이스터에그
-#fsdfsdfsdf
+
+def isInteger(userInput):
+    # 정수 or 실수 예외처리 필요
+    return False
+    
+def isDivision(userInput):
+    if userInput == "/":
+        return True
+    else:
+        return False
+
+def main():
+    errChecker = False
+    result = ""
+
+    while True:
+        userInput = input("Input: ")
+
+        if isDivision(userInput):
+            errChecker = True
+        elif userInput == "=":
+            if errChecker == False:
+                result = eval(result)
+                break
+            else:
+                result = "Error"
+                break
+        else:
+            result +=userInput
+    
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
